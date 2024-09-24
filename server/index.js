8644f2a5c1a75214843bc934da6726528fd17c47
NILESH-2003 <116618534+NILESH-2003@users.noreply.github.com>
2024-09-24 13:08:04 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,13 +6,7 @@ const bodyParser = require("body-parser");
 
 const authRoutes = require("./Routes/auth");
 const noteRoutes = require("./Routes/notes");
-app.use(cors(
-    {
-        origin:["https://deploy-mern-app.vercel.app"],
-        methods:["POST","GET"],
-        credentials:true
-    }
-    ));
+
 const app = express();
 const PORT = 6969;
 
@@ -21,7 +15,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://nira21cs:CpMPVrSgPkKjle2f@cluster0.xal49.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0')
+
 try {
     mongoose.connect(process.env.MONGO_URL);
     console.log("Connection Successfull");
