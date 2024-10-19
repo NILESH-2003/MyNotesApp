e5470fb571a943ae8d36b1aafe1868a0f5984365
NILESH-2003 <116618534+NILESH-2003@users.noreply.github.com>
2024-10-20 03:18:50 +0530

    Update UploadNote.jsx

--- client/src/components/UploadNote.jsx
+++ client/src/components/UploadNote.jsx
@@ -26,7 +26,7 @@ const UploadNote = () => {
       console.log(formData);
 
       const result = await axios.post(
-        "http://localhost:6969/notes/upload",
+        "https://mynotesapp-0r6s.onrender.com/notes/upload",
         formData,
         {
           headers: {
